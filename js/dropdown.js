b82843afca14e9e8e48f0e2f9957432c3644ed6f
xsamhallx <54696339+xsamhallx@users.noreply.github.com>
2021-01-14 16:05:26 +1100

    build update

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,4 +1,5 @@
 function dropDown(x) {
     x.nextElementSibling.classList.toggle("dropdown-close");
     x.nextElementSibling.classList.toggle("dropdown-open");
+    x.lastElementChild.classList.toggle("chevron-invert");
   }
