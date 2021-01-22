8ef711c3b633f7f5fd7ffd92aa3aac27cdfccd4a
xsamhallx <54696339+xsamhallx@users.noreply.github.com>
2021-01-22 15:24:39 +1100

    faq dropdown fix

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,3 +3,9 @@ function dropDown(x) {
     x.nextElementSibling.classList.toggle("block-display");
     x.lastElementChild.classList.toggle("chevron-invert");
   }
+
+  function selfDropDown(x) {
+    x.classList.toggle("no-display");
+    x.classList.toggle("block-display");
+    x.previousElementSibling.lastElementChild.classList.toggle("chevron-invert");
+  }
